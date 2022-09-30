a43ef3adee111cd49c991d6bc91b79084b5afae4
jawed7siddiqui@gmail.com <jawed7siddiqui@gmail.com>
2022-09-30 21:55:13 +0530

    fixed sms

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Alert } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function Home() {
 
@@ -22,21 +23,33 @@ function Home() {
         setOpen(false);
     };
 
-    function sendSMS(e) {
-        e.preventDefault()
-        fetch(`https://2factor.in/API/R1/?module=TRANS_SMS&apikey=e22905d7-a75a-11ec-a4c2-0200cd936042&to=${mobileNumber}&from=DTDTDT&templatename=DTDTDT&var1=${collection}`).then((result) => {
-            result.json().then((resp) => {
-                if (resp.Status === "success") {
-                    setAlertMessageBg('success')
-                    setAlertMessage("SMS Sent!")
-                    handleClick()
-                } else {
-                    setAlertMessageBg('danger')
-                    setAlertMessage(resp.Details)
-                    handleClick()
-                }
-            })
-        })
+    function sendSMS() {
+
+        axios.get(`https://2factor.in/API/R1/?module=TRANS_SMS&apikey=e22905d7-a75a-11ec-a4c2-0200cd936042&to=${mobileNumber}&from=DTDTDT&templatename=DTDTDT&var1=${collection}`).then(response => {
+        console.log(response);
+        });
+       
+        // fetch(`https://2factor.in/API/R1/?module=TRANS_SMS&apikey=e22905d7-a75a-11ec-a4c2-0200cd936042&to=${mobileNumber}&from=DTDTDT&templatename=DTDTDT&var1=${collection}`,{
+        //     method: 'GET',
+        //     headers: {
+        //       'Content-Type': 'application/json',
+        //     }}).then((result) => {
+
+        //         console.log(result);
+                // return false;
+
+            // result.json().then((resp) => {
+            //     if (resp.Status === "success") {
+            //         setAlertMessageBg('success')
+            //         setAlertMessage("SMS Sent!")
+            //         handleClick()
+            //     } else {
+            //         setAlertMessageBg('danger')
+            //         setAlertMessage(resp.Details)
+            //         handleClick()
+            //     }
+            // })
+        // })
     }
 
     return (
@@ -52,7 +65,7 @@ function Home() {
                         <img src="https://www.pngitem.com/pimgs/m/485-4852378_sample-logo-png-transparent-png.png" alt="#ImgNotFound" width="100px" height="70px" />
                     </div>
                     <div>
-                        <form onSubmit={(e) => sendSMS(e)} className='mt-5'>
+                        <form  className='mt-5'>
                             <label for="mobile_number" className="form-label" style={{ fontSize: "18px" }}><b>Mobile Number</b></label>
                             <input type="text" className="form-control mb-3" id="mobile_number" autoFocus style={{ width: "100%", fontSize: "18px" }} minLength="10" maxLength="10" required value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} autoComplete='off' />
                             <label for="new_password" className="form-label" style={{ fontSize: "18px" }}><b>Select Collection</b></label>
@@ -62,7 +75,7 @@ function Home() {
                                 <option value="FTV">FTV</option>
                                 <option value="MF">MF</option>
                             </select>
-                            <button type="submit" className="btn btn-primary w-100 mt-3">Continue</button>
+                            <button type="button" onClick={sendSMS} className="btn btn-primary w-100 mt-3">Continue</button>
                         </form>
                     </div>
                 </div>
